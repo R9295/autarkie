45b8d4a2504341dbe95e635b57e39a25682eae08
aarnav <aarnav@srlabs.de>
2025-03-06 11:18:52 +0100

    lol

---
+++ autarkie/src/fuzzer/libfuzzer.rs
@@ -0,0 +1,18 @@
+#[macro_export]
+macro_rules! libfuzzer_main {
+    ($t: ty) => {
+        fn main() {}
+    }
+}
+
+#[macro_export]
+macro_rules! fuzz_libfuzzer {
+    ($t:ty) => {
+        $crate::impl_input!($t);
+        $crate::libfuzzer_main!($t);
+    };
+    ($t:ty, $closure:expr) => {
+        $crate::impl_input!($t);
+        $crate::libfuzzer_main!($t);
+    };
+}
