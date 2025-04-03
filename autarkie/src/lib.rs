21c50fe9f08bcedd288e6b6b086093ac05daa077
aarnav <aarnav@srlabs.de>
2025-04-03 15:39:38 +0200

    whoops

--- autarkie/src/lib.rs
+++ autarkie/src/lib.rs
@@ -4,9 +4,6 @@
 #[cfg(feature = "autarkie_derive")]
 pub use autarkie_derive::Grammar;
 
-#[cfg(feature = "bincode")]
-pub use serde::{Serialize, Deserialize};
-
 pub use blake3::hash;
 pub use libafl::corpus::CorpusId;
 pub use libafl::inputs::Input;
